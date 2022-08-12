7f2e1b8195fc95de640b234ae6d42ad306ac80e9
Eko Teguh Widodo <id.ekoteguh@gmail.com>
2022-08-12 17:07:43 +0700

    implementasi API delete a book

--- 3-backend-basic/bookshelf-api/src/handlers/book.handler.js
+++ 3-backend-basic/bookshelf-api/src/handlers/book.handler.js
@@ -146,8 +146,27 @@ const updateBookById = (request, h) => {
 }
 
 // API menghapus data buku
-const deleteBook = (request, h) => {
+const deleteBookById = (request, h) => {
+    const { bookId } = request.params;
 
+    const index = books.findIndex((b) => b.id === bookId);
+
+    if (index !== -1) {
+        books.splice(index, 1);
+        const response = h.response({
+            status: 'success',
+            message: 'Buku berhasil dihapus',
+        });
+        response.code(200);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Buku gagal dihapus. Id tidak ditemukan',
+    });
+    response.code(404);
+    return response;
 }
 
 module.exports = {
@@ -155,5 +174,5 @@ module.exports = {
     getBooks,
     getBookById,
     updateBookById,
-    deleteBook,
+    deleteBookById,
 }
--- 3-backend-basic/bookshelf-api/src/routes/book.route.js
+++ 3-backend-basic/bookshelf-api/src/routes/book.route.js
@@ -24,7 +24,7 @@ const router = [
   {
     method: "DELETE",
     path: "/books/{bookId}",
-    handler: bookHandlers.deleteBook,
+    handler: bookHandlers.deleteBookById,
   },
 ];
 
